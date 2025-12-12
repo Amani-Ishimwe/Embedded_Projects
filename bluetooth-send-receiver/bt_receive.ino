79f5c5317f93ae38f961c1f0ea2cf2d1034d631b
Amani Ishimwe <amaniishimwe36@gmail.com>
2025-12-12 22:56:54 +0200

    added the bluetooth

---
+++ bluetooth-send-receiver/bt_receive.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+const int ledPin = 9;
+SoftwareSerial bt(10, 11); 
+void setup() {
+  pinMode(ledPin, OUTPUT);
+  Serial.begin(9600);  
+  bt.begin(9600);
+}
+void loop() {
+  static char command = '0';
+  if (bt.available()) {
+    command = bt.read();
+    Serial.print("Received: ");
+    Serial.println(command);
+  }
+  if (command == '1') {
+    digitalWrite(ledPin, !digitalRead(ledPin)); 
+    command = '0';  
+  }
+}
